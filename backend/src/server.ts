2228f8e43818a71781f28d52ad676ac09ad3640e
brian070 <brianwgatundu@gmail.com>
2025-12-07 14:25:57 +0300

    feat: Implement hash generation

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,11 @@
+import { MultiplierGenerator } from "./services/game/multiplier_generator";
+
 console.log("Velox server");
+
+const multiplier = new MultiplierGenerator();
+
+multiplier.generatedServerSeed();
+multiplier.generateFinalResults({
+  clientSeed: "hellothere    ",
+  clientSeedDetails: [],
+});
--- backend/src/services/game/multiplier_generator.ts
+++ backend/src/services/game/multiplier_generator.ts
@@ -11,6 +11,11 @@ interface MultiplierStateI {
   finalMultiplier: number | null;
 }
 
+interface ClientSeedDetailsI {
+  seed: string;
+  userId: string;
+}
+
 export class MultiplierGenerator {
   private static readonly CONFIG = {
     houseEdge: 3,
@@ -44,5 +49,54 @@ export class MultiplierGenerator {
     this.multiplierState.hashedServerSeed = hashedServerSeed;
 
     return serverSeed;
+  }
+
+  public generateFinalResults(params: {
+    clientSeed: string;
+    clientSeedDetails: ClientSeedDetailsI[];
+  }) {
+    this.validateClientSeed(params.clientSeed);
+
+    this.multiplierState.clientSeed = params.clientSeed.trim();
+    this.multiplierState.clientSeedDetails = params.clientSeedDetails;
+
+    this.generateHash();
+  }
+
+  private generateHash(): string {
+    if (!this.multiplierState.serverSeed) {
+      throw new Error("Server seed was not generated");
+    }
+
+    if (!this.multiplierState.clientSeed) {
+      throw new Error("Client seed is required to generate hash");
+    }
+
+    const combinedSeeds = `${this.multiplierState.serverSeed}${this.multiplierState.clientSeed}`;
+    const hash = crypto
+      .createHash(MultiplierGenerator.CONFIG.hashingAlgorithm)
+      .update(combinedSeeds)
+      .digest(MultiplierGenerator.CONFIG.encodingFormat);
+
+    this.multiplierState.hash = hash;
+
+    console.log(this);
+
+    return hash;
+  }
+
+  private validateClientSeed(clientSeed: string) {
+    if (typeof clientSeed !== "string") {
+      throw new Error(
+        `Client Seed should be a string, got type ${typeof clientSeed}`
+      );
+    }
+    if (!clientSeed.trim()) {
+      throw new Error("Client seed cannot be empty or whitespace only");
+    }
+
+    if (clientSeed.length > 75) {
+      throw new Error("Client seed is too long (max 1000 characters)");
+    }
   }
 }
